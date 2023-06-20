264b35ef6354f8a1ce43e93ff33f85ad87c328fe
Vitalii Khrystov <vitalik_khristov@ukr.net>
2023-06-20 21:18:25 +0300

    Add CommentController. Add action "CreateComment"

---
+++ CommentService/Controllers/CommentController.cs
@@ -0,0 +1,68 @@
+﻿using CommentService.Domain.Enteties;
+using CommentService.Domain.Repositories.Abstract;
+using CommentService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace CommentService.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository commentRepository;
+        private readonly ILogger<CommentController> logger;
+
+        public CommentController(ICommentRepository commentRepository, ILogger<CommentController> logger)
+        {
+            this.commentRepository = commentRepository;
+            this.logger = logger;
+        }
+
+        [HttpPost ("create")]
+        public async Task<IActionResult> CreateComment(CommentModel model)
+        {
+            if (model == null)
+                return NoContent();
+
+            if (ModelState.IsValid)
+            {
+                var comment = new Comment()
+                {
+                    CommentId = Guid.NewGuid().ToString(),
+                    ParrentId = model.ParrentId,
+                    UserId = model.UserId,
+                    CommentText = model.CommentText,
+                    CreatedAt= DateTime.Now,
+                    TopicURL= model.TopicURL
+                };
+                try
+                {
+                    await commentRepository.CreateCommentAsync(comment);
+                    return Ok("Comment created successfully");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError("CreateComment Error: " + ex.Message);
+                    return BadRequest();
+                }
+            }
+            else
+            {
+                var errors = new List<ModelError>();
+                foreach (var modelState in ModelState.Values)
+                {
+                    foreach (var error in modelState.Errors)
+                    {
+                        errors.Add(error);
+                        logger.LogError("ModelState error: \n" + error);
+                    }
+                }
+                return BadRequest(errors);
+            }
+        }
+    }
+}
---
+++ CommentService/Models/CommentModel.cs
@@ -0,0 +1,15 @@
+﻿using CommentService.Domain.Enteties;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommentService.Models
+{
+    public class CommentModel
+    {
+        public string? TopicURL { get; set; }
+        public string? ParrentId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string? CommentText { get; set; }
+    }
+}
